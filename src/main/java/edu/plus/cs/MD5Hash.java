1a85b93c75e6439943bc4fc729a81594cd94aac9
Peter Lindner <peter.lindner2@stud.sbg.ac.at>
2023-05-14 01:40:15 +0200

    implemented md5 algorithm + check two given certificates for a collision

---
+++ src/main/java/edu/plus/cs/MD5Hash.java
@@ -0,0 +1,131 @@
+package edu.plus.cs;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MD5Hash {
+    private static final int INIT_A = 0x67452301;
+    private static final int INIT_B = (int) 0xEFCDAB89L;
+    private static final int INIT_C = (int) 0x98BADCFEL;
+    private static final int INIT_D = 0x10325476;
+
+    private static final int[] SHIFT_AMTS = {
+            7, 12, 17, 22,
+            5, 9, 14, 20,
+            4, 11, 16, 23,
+            6, 10, 15, 21
+    };
+
+    private static final int[] TABLE_T = new int[64];
+
+    static {
+        for (int i = 0; i < 64; i++)
+            TABLE_T[i] = (int) (long) ((1L << 32) * Math.abs(Math.sin(i + 1)));
+    }
+
+    public static byte[] computeMD5(byte[] message, String outputFile) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+
+        int messageLenBytes = message.length;
+        int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
+        int totalLen = numBlocks << 6;
+        byte[] paddingBytes = new byte[totalLen - messageLenBytes];
+        paddingBytes[0] = (byte) 0x80;
+
+        long messageLenBits = (long) messageLenBytes << 3;
+        for (int i = 0; i < 8; i++) {
+            paddingBytes[paddingBytes.length - 8 + i] = (byte) messageLenBits;
+            messageLenBits >>>= 8;
+        }
+
+        int a = INIT_A;
+        int b = INIT_B;
+        int c = INIT_C;
+        int d = INIT_D;
+        int[] buffer = new int[16];
+        for (int i = 0; i < numBlocks; i++) {
+            int index = i << 6;
+            for (int j = 0; j < 64; j++, index++)
+                buffer[j >>> 2] = ((int) ((index < messageLenBytes) ? message[index] : paddingBytes[index - messageLenBytes]) << 24) | (buffer[j >>> 2] >>> 8);
+            int originalA = a;
+            int originalB = b;
+            int originalC = c;
+            int originalD = d;
+            for (int j = 0; j < 64; j++) {
+                int div16 = j >>> 4;
+                int f = 0;
+                int bufferIndex = j;
+                switch (div16) {
+                    case 0:
+                        f = (b & c) | (~b & d);
+                        break;
+
+                    case 1:
+                        f = (b & d) | (c & ~d);
+                        bufferIndex = (bufferIndex * 5 + 1) & 0x0F;
+                        break;
+
+                    case 2:
+                        f = b ^ c ^ d;
+                        bufferIndex = (bufferIndex * 3 + 5) & 0x0F;
+                        break;
+
+                    case 3:
+                        f = c ^ (b | ~d);
+                        bufferIndex = (bufferIndex * 7) & 0x0F;
+                        break;
+                }
+                int temp = b + Integer.rotateLeft(a + f + buffer[bufferIndex] + TABLE_T[j], SHIFT_AMTS[(div16 << 2) | (j & 3)]);
+                a = d;
+                d = c;
+                c = b;
+                b = temp;
+            }
+
+            a += originalA;
+            b += originalB;
+            c += originalC;
+            d += originalD;
+
+            bufferedWriter.write(getHash(buffer));
+            bufferedWriter.write("\n");
+        }
+
+        byte[] md5 = new byte[16];
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            int n = (i == 0) ? a : ((i == 1) ? b : ((i == 2) ? c : d));
+            for (int j = 0; j < 4; j++) {
+                md5[count++] = (byte) n;
+                n >>>= 8;
+            }
+        }
+
+        bufferedWriter.close();
+        return md5;
+    }
+
+    private static String getHash(int[] h){
+        // Convert the hash values to a byte array
+        byte[] hashBytes = new byte[20];
+        for (int i = 0; i < 5; i++) {
+            hashBytes[i * 4] = (byte) ((h[i] >> 24) & 0xff);
+            hashBytes[i * 4 + 1] = (byte) ((h[i] >> 16) & 0xff);
+            hashBytes[i * 4 + 2] = (byte) ((h[i] >> 8) & 0xff);
+            hashBytes[i * 4 + 3] = (byte) (h[i] & 0xff);
+        }
+
+        // Convert the hash bytes to a hex string
+        StringBuilder hexString = new StringBuilder();
+        for (byte hashByte : hashBytes) {
+            String hex = Integer.toHexString(0xff & hashByte);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+
+        // Print the SHA-1 hash
+        //System.out.println(hexString.toString());
+        return hexString.toString();
+    }
+}
---
+++ src/main/java/edu/plus/cs/X509CertificateSubjectHash.java
@@ -0,0 +1,55 @@
+package edu.plus.cs;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+public class X509CertificateSubjectHash {
+
+    public static void main(String[] args) {
+        String certificateFile1 = "src/main/resources/TargetCollidingCertificate1.cer";
+        String certificateFile2 = "src/main/resources/TargetCollidingCertificate2.cer";
+        String outputFile1 = "src/main/resources/outputHashCert1.txt";
+        String outputFile2 = "src/main/resources/outputHashCert2.txt";
+
+        calculateSubjectHash(certificateFile1, outputFile1);
+        calculateSubjectHash(certificateFile2, outputFile2);
+    }
+
+    private static void calculateSubjectHash(String certificateFile, String outputFile) {
+        try (FileInputStream fis = new FileInputStream(certificateFile)) {
+            X509Certificate certificate = loadCertificate(fis);
+
+            byte[] subjectHash = generateSubjectHash(certificate, outputFile);
+            String subjectHashHex = bytesToHex(subjectHash);
+            System.out.println("Subject Hash Cert1: " + subjectHashHex);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException | CertificateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static X509Certificate loadCertificate(FileInputStream fis) throws CertificateException {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        return (X509Certificate) cf.generateCertificate(fis);
+    }
+
+    public static byte[] generateSubjectHash(X509Certificate certificate, String outputFile)
+            throws CertificateEncodingException, IOException {
+        byte[] encodedSubject = certificate.getTBSCertificate();
+        return MD5Hash.computeMD5(encodedSubject, outputFile);
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02X", b));
+        }
+        return result.toString();
+    }
+}
